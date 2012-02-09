5a122095d80d976646afa4be55918935bbfed093
mike castleman <m@mlcastle.net>
2012-02-09 15:37:11 -0500

    try to auth

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ To deploy to heroku:
 
 ```
 heroku create -s cedar mup-copier
-heroku config:add RACK_ENV=production
+heroku config:set RACK_ENV=production
+heroku config:set SESSION_SECRET=`dd if=/dev/urandom bs=30 count=1 | openssl base64`
 git push heroku master
 ```
--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
 require 'sinatra'
 require 'haml'
 require 'sass'
+require 'omniauth'
+require 'omniauth-meetup'
+require 'pp'
 
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :views, "views"
 set :public_folder, 'static'
 
-enable :sessions
+use Rack::Session::Cookie, :session => ENV['SESSION_SECRET']
+use OmniAuth::Builder do
+  provider :meetup, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET']
+end
 
 def skip_session
   request.session_options[:skip] = true
@@ -22,4 +28,10 @@ end
 
 get '/' do
   haml :index, :format => :html5
+end
+
+get '/auth/:name/callback' do
+  auth = request.env['omniauth.auth']
+  content_type 'text/plain', :charset => 'utf-8'
+  pp(auth)
 end
