c1c7a9d24ecb618e1e3162fbf7fb242d7931ff09
mike castleman <m@mlcastle.net>
2012-02-11 18:26:48 -0500

    DRY

--- lib/service.rb
+++ lib/service.rb
@@ -1,21 +1,7 @@
 module Service
   module ClassMethods
     def get(svc, user)
-      find_klass(svc).new(user["services"][svc])
-    end
-
-    # I used to know how to be more DRY about this in Ruby…
-    def find_klass(svc)
-      case svc
-        when "flickr"
-          Service::Flickr
-        when "facebook"
-          Service::Facebook
-        when "dropbox"
-          Service::Dropbox
-        else
-          raise "unknown service #{svc}"
-      end
+      const_get(svc.to_s.capitalize).new(user["services"][svc])
     end
   end
 
